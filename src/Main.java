aee84edd4f071713f0ee5663fb4199f742333d16
omerbariskayaalp <omerbariskayaalp@gmail.com>
2023-07-17 23:18:44 +0300

    MiniProject2

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        char harf ='j';
-        char[] kalın_harfler = {'a', 'A', 'ı', 'I', 'o', 'O', 'u', 'U'};
-        char[] ince_harfler = {'e', 'i', 'ö', 'ü', 'E', 'İ', 'Ö', 'Ü'};
+        char letter ='j';
+        char[] bold_letter = {'a', 'A', 'ı', 'I', 'o', 'O', 'u', 'U'};
+        char[] thin_letter = {'e', 'i', 'ö', 'ü', 'E', 'İ', 'Ö', 'Ü'};
    // bu işlemi case ile de yapılabilir.
         for (int i=0;i<8;i++){
-            if (harf==kalın_harfler[i]){
-                System.out.println("Bu karakter bir kalın ve sesli harftir.");
+            if (letter==bold_letter[i]){
+                System.out.println("This character is a bold and vowel.");
                 break;
             }
-            else if (harf==ince_harfler[i]){
-                System.out.println("Bu karakter ince ve sesli harftir");
+            else if (letter==thin_letter[i]){
+                System.out.println("This character is thin and vowel.");
                 break;
             }
             else{
-                System.out.println("Bu karakter sesli harf değildir.");
+                System.out.println("This character is not a vowel.");
                 break;
             }
         }
-
-
     }
 }
